d2601fcd690afd34a0e003ae0f0a656a1e75bc08
LLxD <limalucas454@gmail.com>
2025-07-05 18:11:28 -0300

    chore: adding ezforms

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,27 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
-    
+  },
+  ezforms: {
+    config: {
+      captchaProvider: {
+        name: "none",
+      },
+      notificationProviders: [],
+      enableDeveloperMode: true,
+      publicAccess: true,
+    },
+  },
 });
